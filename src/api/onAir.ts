5848c3c80e2b3b64c497eb5a636e44704b626d07
Denys Symanko <d.simanko@bossgs.com>
2023-10-09 12:42:21 +0300

    ts hotfix

--- src/api/onAir.ts
+++ src/api/onAir.ts
@@ -1,7 +1,7 @@
 import { httpClient } from '@/lib/http-client/';
 import type { Composition } from '@/components/CompositionItem.vue';
 
-export const getAllCompositions = async (): { nowplaying: Composition[] } => {
+export const getAllCompositions = async (): Promise<{ nowplaying: Composition[] }> => {
   const { data } = await httpClient.get('/compositions');
 
   return data;
